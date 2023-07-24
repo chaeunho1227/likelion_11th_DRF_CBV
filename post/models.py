2816850527f935f9003ce155713bf776e6d24b14
chaeunho1227 <chaeunho2002@gmail.com>
2023-07-24 16:23:10 +0900

    4week 과제

--- post/models.py
+++ post/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 def image_upload_path(instance, filename):
     return f'{instance.id}/{filename}'
@@ -11,18 +11,18 @@ class Tag(models.Model):
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
-    writer = models.CharField(max_length=50)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     tag = models.ManyToManyField(Tag,blank=True)
     image = models.ImageField(upload_to=image_upload_path,blank=True, null= True)
-
+    likes = models.PositiveSmallIntegerField(default=0)
 
 class Comment(models.Model):
     id = models.AutoField(primary_key=True)
     post = models.ForeignKey(Post, blank=False, null=False, on_delete=models.CASCADE, related_name='comments')
-    writer = models.CharField(max_length=50)
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(max_length=200)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
--- post/views.py
+++ post/views.py
@@ -1,5 +1,9 @@
 from rest_framework import viewsets, mixins
 from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser,IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.decorators import action
+from .permissions import IsOwnerOrReadOnly
 from django.shortcuts import get_object_or_404
 
 from .models import *
@@ -8,7 +12,19 @@ from .serializers import *
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return PostListSerializer
+        return PostSerializer
+    
+    def get_permissions(self):
+        if self.action in ['update','destroy','partial_update']:
+            return [IsOwnerOrReadOnly()]
+        return[]
+
+    def get_object(self):
+        obj = super().get_object()
+        return obj
     
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
@@ -38,13 +54,40 @@ class PostViewSet(viewsets.ModelViewSet):
         post.tag.clear()
         self.handle_tags(post)
 
+    @action(methods=['GET'],detail=False)
+    def biggest_likes_3(self, request):
+        big_3_posts = Post.objects.all().order_by('-likes')[:3]
+        big_3_posts_serializer = PostListSerializer(big_3_posts,many=True)
+        return Response(big_3_posts_serializer.data)
+    
+    @action(methods=['GET'],detail=True)
+    def like(self, requset, pk=None):
+        like_post = self.get_object()
+        like_post.likes += 1
+        like_post.save(update_fields=['likes'])
+        return Response()
+
 class CommentViewSet(viewsets.GenericViewSet,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
 
+    def get_permissions(self):
+        if self.action in ['update','destroy','partial_update']:
+            return [IsOwnerOrReadOnly()]
+        return[]
+
+    def get_object(self):
+        obj = super().get_object()
+        return obj
+    
 class PostCommentViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
-    queryset = Comment.objects.all()
     serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        post = self.kwargs.get("post_id")
+        queryset = Comment.objects.filter(post_id=post)
+        return queryset
 
     def list(self, request, post_id=None):
         post = get_object_or_404(Post, id=post_id)
